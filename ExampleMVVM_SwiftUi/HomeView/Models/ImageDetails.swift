debc15e7327f19ff20a17d879b11e8d55dcd2318
nabiCoder <nabiulli_n@vk.com>
2024-01-18 14:36:47 +0500

    Creating a home screen. Created a data model

---
+++ ExampleMVVM_SwiftUi/HomeView/Models/ImageDetails.swift
@@ -0,0 +1,13 @@
+struct ImageDetails: Decodable {
+    
+    let albumID: Int
+    let id: Int
+    let title: String
+    let url, thumbnailURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case albumID = "albumId"
+        case id, title, url
+        case thumbnailURL = "thumbnailUrl"
+    }
+}
