50a18b04f0079cda9d1d49ab9b6317e8b42fd7a0
nabiCoder <nabiulli_n@vk.com>
2024-01-18 14:25:54 +0500

    Created NetworkDataFetch

---
+++ ExampleMVVM_SwiftUi/Application/Networks/NetworkDataFetch.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+final class NetworkDataFetch {
+
+    static func getData(id: Int) async throws -> ImageDetails {
+        let decoder = JSONDecoder()
+        do {
+            let response = try await URLSession.shared.request(.image(id: id))
+            let result = try decoder.decode(ImageDetails.self, from: response.0)
+            
+            return result
+        } catch {
+            throw NetworkError.canNotParseData
+        }
+    }
+}
